3ab2f1dea218b66116ca3400f493e301bba16161
Christopher J. Bottaro <cjbottaro@alumni.cs.utexas.edu>
2011-07-27 18:41:54 -0400

    Fixed ConnectionPool.

--- lib/active_record/connection_adapters/em_postgresql_adapter.rb
+++ lib/active_record/connection_adapters/em_postgresql_adapter.rb
@@ -71,6 +71,11 @@ module ActiveRecord
       end
 
       def clear_stale_cached_connections!
+
+        # If we aren't using any fiber pools, then don't run this code otherwise it
+        # will checkin *all* connections and clear the @reserved_connections hash.
+        return if ActiveRecord::ConnectionAdapters.fiber_pools.empty?
+        
         cache = @reserved_connections
         keys = Set.new(cache.keys)
 
@@ -93,12 +98,42 @@ module ActiveRecord
         Fiber.current.object_id
       end
 
-      def checkout_and_verify(c)
+      # The next three methods (#checkout_new_connection, #checkout_existing_connection and #checkout_and_verify) require modification.
+      # The reason is because @connection_mutex.synchronize was modified to do nothing, which means #checkout is unguarded.  It was
+      # assumed that was ok because the current fiber wouldn't yield during execution of #checkout, but that is untrue.  Both #new_connection
+      # and #checkout_and_verify will yield the current fiber, thus allowing the body of #checkout to be accessed by multiple fibers at once.
+      # So if we want this to work without a lock, we need to make sure that the variables used to test the conditions in #checkout are
+      # modified *before* the current fiber is yielded and the next fiber enters #checkout.
+
+      def checkout_new_connection
+
+        # #new_connection will yield the current fiber, thus we need to fill @connections and @checked_out with placeholders so
+        # that the next fiber to enter #checkout will take the appropriate action.  Once we actually have our connection, we
+        # replace the placeholders with it.
+
+        @connections << current_connection_id
+        @checked_out << current_connection_id
+
+        c = new_connection
+
+        @connections[@connections.index(current_connection_id)] = c
+        @checked_out[@checked_out.index(current_connection_id)] = c
+
+        checkout_and_verify(c)
+      end
+
+      def checkout_existing_connection
+        c = (@connections - @checked_out).first
         @checked_out << c
+        checkout_and_verify(c)
+      end      
+
+      def checkout_and_verify(c)
         c.run_callbacks :checkout
         c.verify!
         c
       end
+
     end # ConnectionPool
 
     class EMPostgreSQLAdapter < ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
