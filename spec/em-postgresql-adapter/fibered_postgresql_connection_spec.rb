676bbb5cd5d25a10dac7977affc9a7e5c8fe46db
Laust Rud Jacobsen <laust@object.io>
2011-12-13 15:46:31 +0100

    FibredPostgresConnection: spec for running basic queries inside eventmachine

--- spec/em-postgresql-adapter/fibered_postgresql_connection_spec.rb
+++ spec/em-postgresql-adapter/fibered_postgresql_connection_spec.rb
@@ -15,8 +15,71 @@ describe EM::DB::FiberedPostgresConnection, 'plain integration into active_recor
   end
 
   it "can connect outside of eventmachine" do
-    result = ActiveRecord::Base.connection.execute('select 42')
+    result = ActiveRecord::Base.connection.execute('SELECT 42')
     result.values.should == [['42']]
+  end
+
+end
+
+describe EM::DB::FiberedPostgresConnection, 'integration with active_record inside an event_machine context', :type => :eventmachine do
+  SLEEP_TIME = 0.2
+
+  before(:each) do
+    ActiveRecord::Base.establish_connection(database_connection_config)
+  end
+
+  it "can establish a connection and execute a query while running inside eventmachine" do
+    result = nil
+    em {
+      Fiber.new {
+        result = ActiveRecord::Base.connection.execute('SELECT 42').values
+        done
+      }.resume
+    }
+    result.should == [['42']]
+  end
+
+  it "can execute multiple queries on parallel fibers concurrently" do
+    start = Time.now
+    results = []
+    em {
+      EM::PeriodicTimer.new(0.01) { print "." }
+
+      Fiber.new {
+        results << ActiveRecord::Base.connection.execute("SELECT pg_sleep(#{SLEEP_TIME}), 42").values
+        done if results.length == 2
+      }.resume
+
+      Fiber.new {
+        results << ActiveRecord::Base.connection.execute("SELECT pg_sleep(#{SLEEP_TIME}), 43").values
+        done if results.length == 2
+      }.resume
+    }
+
+    (Time.now - start).should be_within(SLEEP_TIME * 0.15).of(SLEEP_TIME)
+  end
+
+
+  it "can collect values from multiple queries on a single fiber" do
+    results = []
+    em {
+      Fiber.new {
+        results << ActiveRecord::Base.connection.execute("SELECT 42").values
+        results << ActiveRecord::Base.connection.execute("SELECT 43").values
+        done
+      }.resume
+    }
+    results.flatten.should include('42')
+    results.flatten.should include('43')
+  end
+
+  it "raises an error if not called inside a fiber" do
+    em {
+      expect {
+        ActiveRecord::Base.connection.execute('SELECT 42')
+      }.to raise_error(ActiveRecord::StatementInvalid, /FiberError/)
+      done
+    }
   end
 
 end
