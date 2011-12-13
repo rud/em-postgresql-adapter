da95893b522f9966732b6d92d1fcae1d5b237431
Laust Rud Jacobsen <laust@object.io>
2011-12-13 14:39:28 +0100

    Initial spec: behavior outside of an EventMachine context, integrated with ActiveRecord

--- spec/em-postgresql-adapter/fibered_postgresql_connection_spec.rb
+++ spec/em-postgresql-adapter/fibered_postgresql_connection_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
-describe EM::DB::FiberedPostgresConnection do
+describe EM::DB::FiberedPostgresConnection, 'plain integration into active_record' do
+
+  before :each do
+    ActiveRecord::Base.establish_connection(database_connection_config)
+  end
+
+  it "is a sane database.yml file" do
+    database_connection_config['adapter'].should == 'em_postgresql'
+  end
+
+  it "uses an identifiable driver" do
+    ActiveRecord::Base.connection.should be_instance_of(ActiveRecord::ConnectionAdapters::EMPostgreSQLAdapter)
+  end
+
+  it "can connect outside of eventmachine" do
+    result = ActiveRecord::Base.connection.execute('select 42')
+    result.values.should == [['42']]
+  end
+
 end
